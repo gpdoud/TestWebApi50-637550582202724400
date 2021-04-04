b5c4cedcb783b8fc8e41c023c9ba8223990db732
Greg Doud <gpdoud@gmail.com>
2021-04-04 11:07:02 -0400

    Committed by GitUtil

---
+++ TestWebApi50/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApi50.Models {
+    public class AppDbContext : DbContext {
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) {
+        }
+    }
+}
---
+++ TestWebApi50/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TestWebApi50.Models {
+    public class Customer {
+
+        public int Id { get; set; } = default;
+        public string Name { get; set; } = default;
+
+        public Customer() {
+        }
+    }
+}
